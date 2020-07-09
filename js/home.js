a5d9ad3566fe28fd0c8f3334c307b51b95a80571
codernuub <sardarbhai6@gmail.com>
2020-07-10 00:28:00 +0530

    desgin test

---
+++ js/home.js
@@ -0,0 +1,26 @@
+function menuToggle() {
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector(".nav");
+  console.log(nav.clientHeight);
+  menu.classList.toggle("menu-active");
+  nav.classList.toggle("nav-active");
+}
+
+
+
+window.onload = function () {
+  window.addEventListener("scroll", () => showBottom((window.pageYOffset > window.innerHeight)));
+};
+
+let prev = false;
+function showBottom(ok) {
+  const scrol = document.querySelector(".UP");
+  if (ok) {
+    if (prev) return;
+    scrol.classList.add("activeUP");
+    prev = ok;
+  } else {
+    prev && scrol.classList.remove("activeUP");
+    prev = ok;
+  }
+}
